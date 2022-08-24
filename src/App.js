51300bfad97cfbcbe532f22ebd9e418fe1f3bf96
GUchechi <godswilluchendu7@gmail.com>
2022-08-24 02:24:52 +0100

    added useEffect

--- src/App.js
+++ src/App.js
@@ -1,9 +1,24 @@
 
+import { useEffect, useState } from 'react';
 import './App.css';
+import Axios from 'axios';
 
 function App() {
+const [url, SetUrl] = useState("")
+
+useEffect(() => {
+  Axios.get("https://aws.random.cat/meow").then((url) => {
+    SetUrl(url.data["file"])
+  })
+},[])
+
   return (
     <div className="App">
+    {
+      url !== ""  &&(
+        <img src ={ url} alt="cat"/> 
+      )
+    }
       
     </div>
   );
